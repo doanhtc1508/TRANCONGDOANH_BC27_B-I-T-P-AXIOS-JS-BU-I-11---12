213a859677e4327448685f46a6b516b8783d5b81
doanh11 <trancongdoanh150896@gmail.com>
2022-06-25 19:02:20 +0700

    fixdeader

--- layout_API_OUR TECH/scss/js/style.js
+++ layout_API_OUR TECH/scss/js/style.js
@@ -1,6 +1,5 @@
 window.addEventListener("scroll", function () {
   var header = document.querySelector(".header");
-  var logo = document.querySelector(".logo");
+
   header.classList.toggle("sticky", window.scrollY > 0);
-  logo.classList.toggle("img-2", window.scrollY > 0);
 });
